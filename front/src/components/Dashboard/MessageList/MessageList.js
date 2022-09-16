c2077645057e1d0bace1b4a18b7940891462f6ee
Bardock <86751142+JorgeSaicoski@users.noreply.github.com>
2022-09-16 18:26:59 -0300

    Message automatic generation

--- front/src/components/Dashboard/MessageList/MessageList.js
+++ front/src/components/Dashboard/MessageList/MessageList.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useParams, Link} from "react-router-dom";
 import OpenMessage from "../CardMessages/OpenMessage/OpenMessage";
 import NewMessage from "../CardMessages/NewMessage/NewMessage";
 import ReadedMessage from "../CardMessages/ReadedMessage/ReadedMessage";
+import data from "../../../data/data";
+const db = data()
 
 export default function MessageList(){
+    const mainMessage = useParams()
+    const [messages, setMessages] = useState([])
+    useEffect(()=>{
+        setMessages(db)
+    })
     return(
         <ul className="mt-6 w-full">
-            <OpenMessage title={"Titulo"} name={"Jorge"} date={"27/07/1995"}></OpenMessage>
-            <NewMessage title={"Titulo"} name={"Jorge"} date={"27/07/1995"}></NewMessage>
-            <ReadedMessage name={"Jorge"} date={"27/07/1995"} title={"Titulo"} ></ReadedMessage>
+            {messages.map(message=> `${message.id}` === mainMessage.messageId?
+                <Link to={`/${message.id}`}><OpenMessage key={message.id} title={message.title} name={message.name} date={message.date}></OpenMessage></Link>
+                :
+                message.new? <Link to={`/${message.id}`}><NewMessage key={message.id} title={message.title} name={message.name} date={message.date}></NewMessage></Link>
+
+                : <Link to={`/${message.id}`}><ReadedMessage key={message.id} title={message.title} name={message.name} date={message.date} ></ReadedMessage></Link>
+            )}
+
         </ul>
     )
 }
--- front/src/components/Main/MainMessage.js
+++ front/src/components/Main/MainMessage.js
@@ -2,12 +2,12 @@ import React, {useEffect, useState} from "react";
 import Dashboard from "../Dashboard/Dashboard";
 import Navbar from "../Navbar/Navbar";
 import Message from "../Message/Message";
-import data from "../../data/data";
 
-const db = data()
+
+
 
 export default function MainMessage(){
-    const [message, setMessages] = useState(db)
+
     
 
     return (
